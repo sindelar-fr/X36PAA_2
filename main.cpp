8c051ab3cf422d71035d5418f246117e697f0086
František Šindelář <sindelar.fr@gmail.com>
2009-10-20 13:30:52 +0200

    x36PAA_2 initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/* 
+ * File:   main.cpp
+ * Author: franta
+ *
+ * Created on 20. říjen 2009, 13:28
+ */
+
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
